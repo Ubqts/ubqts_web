d39b36d03be4c8b9bbb93e57efb5fe2224b9852d
Sam Lin <samklin33@gmail.com>
2024-09-02 22:21:42 +0800

    change strcture

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import { AdProvider } from "@/app/context/Ads";
-import { NewsProvider } from "@/app/context/News";
-import { ProductProvider } from "@/app/context/Products";
+import { AdProvider } from "@/src/context/Ads";
+import { NewsProvider } from "@/src/context/News";
+import { ProductProvider } from "@/src/context/Products";
 
 type Props = {
     children: React.ReactNode;
