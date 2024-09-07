77ecf3a611bd06a3e2015f2bc53315e3963dd40d
olabao <051710a@gmail.com>
2024-09-07 18:38:05 +0800

    Finish RWD for news page

--- src/components/news_item.tsx
+++ src/components/news_item.tsx
@@ -5,8 +5,6 @@ export default function NewsItem() {
         <div className="newsItem prevent-select">
             <div className="newsImg"><img src="https://picsum.photos/300/200?random=1" alt="1" /></div>
             <div className="newsInfo">
-                {/* <div className="newsTitle">標題</div>
-                <p className="newsContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quas.</p> */}
                 <div className="newsTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, facilis!</div>
                 <p className="newsContent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sed fuga repellat dolorem libero, autem aut optio quibusdam soluta voluptas, fugiat, officiis nihil eum! Cumque, voluptatibus, est ea atque tempore suscipit rem ex perferendis maxime consequatur nihil, officiis voluptatum fugit illum? Deleniti, adipisci voluptate, laborum assumenda similique aspernatur, qui porro quaerat deserunt magni dolore facere sint ipsa doloremque veniam nemo!</p>
             </div>
