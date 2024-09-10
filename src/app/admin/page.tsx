3e08aa58722e8ebd764f7eb0999238fef7d6e113
olabao <051710a@gmail.com>
2024-09-11 00:08:44 +0800

    Add admin page with basic-only styling

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,23 @@
-'use client';
-import { AdContext } from '../../context/Ads'
-import { NewsContext } from '../../context/News'
-import { ProductContext } from '../../context/Products'
-import { useRouter } from 'next/navigation'
-import React, { useContext } from 'react'
+import "./page.css";
 
-import Header from '../../components/header'
 
-export default function Page() {
-    const { ads } = useContext(AdContext);
-    const { news } = useContext(NewsContext);
-    const { products } = useContext(ProductContext);
-    const router = useRouter();
-
+export default function Admin() {
     return (
-        <>
-        </>
-    );
+        <div>
+            <div className="loginBox prevent-select">
+                <p style={{ margin: "0 auto 25px auto", fontWeight: "bold" }}>管理員登入</p>
+                <input
+                    type="text"
+                    className="userName"
+                    placeholder='帳號'
+                />
+                <input
+                    type="password"
+                    className="userPwd"
+                    placeholder='密碼'
+                />
+                <div className='loginBtn'>登入</div>
+            </div>
+        </div>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,41 +5,29 @@ import BootstrapCarousel from "../components/bootstrap_carousel";
 export default function Home() {
     return (
         <div className="container prevent-select">
-            {/* <div className="carousel">
-                <div className="carouselContainer">
-                    <img src="https://picsum.photos/1700/600?random=1" alt="1" />
-                    <img src="https://picsum.photos/1700/600?random=2" alt="2" />
-                    <img src="https://picsum.photos/1700/600?random=3" alt="3" />
-                </div>
-            </div>
-            <div className="carouselUI">
-                <div className="btn prev">&lt;</div>
-                <div className="indices">
-                    <div className="index-item current"></div>
-                    <div className="index-item"></div>
-                    <div className="index-item"></div>
-                </div>
-                <div className="btn next">&gt;</div>
-            </div> */}
             <BootstrapCarousel />
 
             <div className="blankBanner" />
 
             <div className="content origin">
-                <h1>公司起源</h1>
+                <h1>公司簡介</h1>
                 <div className="imgWithText">
-                    <img src="https://picsum.photos/400?random=4" alt="1" />
+                    <img src="https://picsum.photos/400?random=4" alt="companyIntro" />
                     <div>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa in fugit accusantium enim, porro eius. Dignissimos soluta, necessitatibus, distinctio tempora aut tenetur amet, alias atque provident quae veniam quis.</p>
-                        <p>Accusantium explicabo qui quia mollitia, ab deserunt inventore odio ipsam expedita ratione cumque nihil corrupti consequuntur delectus fugit maiores quidem eligendi earum reiciendis! Optio quod tempore maiores dolore quaerat.</p>
-                        <p>Sint veritatis, architecto tenetur illo fuga et sapiente ad culpa iure velit unde enim doloremque magnam molestiae dolore eligendi, dolorem magni tempore praesentium suscipit minus delectus? Quibusdam, ullam eligendi.</p>
-                        <p>Quaerat enim optio magnam illo id. Deserunt suscipit tenetur atque impedit voluptatem nam aperiam labore maxime sint, magnam unde delectus quae, error amet, corrupti rerum consequuntur veniam corporis doloremque.</p>
-                        <p>Nobis non hic vero provident porro ad eius est consequuntur! Minima, commodi architecto nam natus sapiente similique nihil ut necessitatibus non repellat provident, et velit! Exercitationem est illum maiores!</p>
-                        <p>Animi quae est fugiat harum quidem vel perspiciatis, distinctio ad, corrupti vitae expedita reiciendis hic ipsum culpa, blanditiis optio eaque. Illum totam, nihil aliquid fuga quaerat deserunt ipsa nostrum!</p>
-                        <p>Quibusdam maxime iusto repellendus nam harum, vitae odit eligendi! Non porro facilis ducimus esse, voluptates beatae assumenda nihil quisquam, maxime expedita molestiae ea similique sunt qui, corrupti aperiam officiis!</p>
+                        <p>洲通能源承測試設備業界知名廠商-佳優科技技術及團隊，於2023年正式成立，2024年正式合併、由洲通能源將設備業務擴展至東南亞及歐美，於這期間產品效能及技術服務深得國內外客戶肯定及支持。</p>
+                        <p>近年聽取品牌客戶、系統廠、高等教育學校及研究機構對其需求建議，開發符合客戶期待高效、高精確度及成本效益之自動化測試設備及監控系統，客戶滿意是我們自始至終目標。</p>
+                        <p>洲通能源開發產品皆為自有品牌、合作研發、生產製造與整合行銷，未來持續提供客戶對設備需求及更完善技術服務。</p>
+                        <p>並提供完成行銷通路及良好售後服務滿足客戶因近期第三地生產困境</p>
+                        <p>洲通自動化測試設備:</p>
+                        <p className="listItems">
+                            (1)BAT系列自動測試系統→可充放電電池組開發、效能活化、性能分類、電池組加工、電池組基板、、等。<br />
+                            (2)DAS系列模組化自動化監控系統 →適合依客戶需求整合成自動化測試設備或自動化監控系統。<br />
+                            (3)ATS多用途卡式設備。<br />
+                            (4)協同開發&非標產品研發
+                        </p>
                     </div>
                 </div>
-            </div>
+            </div >
 
             <div className="blankBanner" />
 
@@ -87,6 +75,6 @@ export default function Home() {
 
             <div className="blankBanner" />
             <div className="blankBanner" />
-        </div>
+        </div >
     );
 }
