47040ae6a0f54a2ec6f9b34ecd8cf505b4310152
Sam Lin <samklin33@gmail.com>
2024-09-14 11:25:10 +0800

    finish new_product page

--- src/app/new_product/page.tsx
+++ src/app/new_product/page.tsx
@@ -1,6 +1,42 @@
+'use client';
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 import "./page.css";
+import useProducts from "@/src/hooks/useProducts";
 
 export default function NewProduct() {
+    const router = useRouter();
+    const { postProducts } = useProducts();
+    const [ productName, setProductName ] = useState("");
+    const [ productDescription, setproductDescription ] = useState("");
+    const [ picture, setPicture ] = useState("");
+
+    const handleSave = () => {
+        if (!productName)   {
+            alert("產品名稱不得為空！");
+        }
+        if (!picture)   {
+            alert("請輸入產品圖片網址！");
+        }
+        if (!productDescription)    {
+            alert("產品敘述不得為空！");
+        }
+        try {
+            postProducts({
+                name: productName, 
+                picture: picture, 
+                description: productDescription, 
+                language: "zh-tw"
+            });
+            alert("產品新增成功！");
+            router.push("/");
+        } catch(error) {
+            alert("產品新增失敗！");
+            console.log(error);
+        }
+    }
+
     return (
         <div className="container">
             <div className="banner">
@@ -8,13 +44,13 @@ export default function NewProduct() {
             </div>
             <div className="blankBanner" />
             <div className="content">
-                <input className="title" placeholder="產品名稱" />
-                <input className="img" placeholder="產品圖片網址" />
-                <textarea className="description" placeholder="產品描述" /></div>
+                <input className="title" placeholder="產品名稱" onChange={(e) => setProductName(e.target.value)}/>
+                <input className="img" placeholder="產品圖片網址" onChange={(e) => setPicture(e.target.value)}/>
+                <textarea className="description" placeholder="產品描述" onChange={(e) => setproductDescription(e.target.value)}/></div>
             <div className="blankBanner" />
             <div className="btnContainer prevent-select">
-                <div className="add">新增</div>
-                <div className="cancel">取消</div>
+                <div className="add" onClick={() => handleSave()}>新增</div>
+                <div className="cancel" onClick={() => router.push("/")}>取消</div>
             </div>
             <div className="blankBanner" />
             <div className="blankBanner" />
