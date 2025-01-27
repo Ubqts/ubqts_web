f9f6b6ac0fb98e4a4f24180960afa6e057bc878e
olabao <051710a@gmail.com>
2025-01-28 01:30:03 +0800

    Add loading animation

---
+++ src/components/loading.tsx
@@ -0,0 +1,18 @@
+import "./loading.css";
+
+
+export default function Loading() {
+    return (
+        <div className="loadingContainer">
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+            <div className="loadingItem"></div>
+        </div>
+    );
+}
