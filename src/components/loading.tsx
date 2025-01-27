1ace733a2e09890023937ba26137076bf54a368f
olabao <051710a@gmail.com>
2025-01-28 01:42:48 +0800

    Altered the loading animation

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,18 +1,32 @@
 import "./loading.css";
 
+import React, { useEffect, useState } from "react";
+
 
 export default function Loading() {
+    const [dots, setDots] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDots((dots) => (dots + 1) % 4);
+        }, 500);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="loadingContainer">
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
-            <div className="loadingItem"></div>
+        <div>
+            <p className="loadingText">系統處理中，請稍後{".".repeat(dots)}</p>
+            <div className="loadingContainer">
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+                <div className="loadingItem"></div>
+            </div>
         </div>
     );
 }
